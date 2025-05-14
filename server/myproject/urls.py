bf8678cf62dd02d0c7ad02518ed67e9762248367
Topo0308 <toponelson97@gmail.com>
2025-05-14 19:54:39 +0000

    mise a jours de mes fichiers pour voir me connecte a tous les ports okay

--- server/myproject/urls.py
+++ server/myproject/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('your_app.urls')),
+   """ path('', include('your_app.urls')),
+   ²:"""
     path('', include('django_prometheus.urls')),
 ]
