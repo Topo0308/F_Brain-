48205789e4209168b5bce36deaaac01096ae844f
Topo0308 <toponelson97@gmail.com>
2025-05-28 22:00:36 +0200

    Backend sans rest framework

--- server/trajets/views.py
+++ server/trajets/views.py
@@ -1,19 +1,26 @@
-from rest_framework import viewsets, permissions
-from .models import Trajet, Reservation
-from .serializers import TrajetSerializer, ReservationSerializer
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Trajet
+from .forms import TrajetForm
 
-class TrajetViewSet(viewsets.ModelViewSet):
-    queryset = Trajet.objects.all()
-    serializer_class = TrajetSerializer
-    permission_classes = [permissions.IsAuthenticated]
+def trajets_list(request):
+    trajets = Trajet.objects.all().order_by('-date')
+    return render(request, 'trajets/home.html', {'trajets': trajets})
 
-    def perform_create(self, serializer):
-        serializer.save()
+@login_required
+def dashboard(request):
+    trajets = Trajet.objects.filter(conducteur=request.user)
+    return render(request, 'trajets/dashboard.html', {'trajets': trajets})
 
-class ReservationViewSet(viewsets.ModelViewSet):
-    queryset = Reservation.objects.all()
-    serializer_class = ReservationSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save()
+@login_required
+def create_trajet(request):
+    if request.method == 'POST':
+        form = TrajetForm(request.POST)
+        if form.is_valid():
+            trajet = form.save(commit=False)
+            trajet.conducteur = request.user
+            trajet.save()
+            return redirect('dashboard')
+    else:
+        form = TrajetForm()
+    return render(request, 'trajets/create_trajet.html', {'form': form})
