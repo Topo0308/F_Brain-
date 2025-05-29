7194f72f4a1ba7c5a634f231cb611a7951729e1d
Topo0308 <toponelson97@gmail.com>
2025-05-29 15:25:32 +0200

    REMARAGE DE DJANGO

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-admin.site.unregister(User)
-admin.site.register(User)
-
-
-# users/apps.py
-from django.apps import AppConfig
-
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    pass
