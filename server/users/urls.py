b372a4a80c95bdb297667f0ed7331b439f615ada
Topo0308 <toponelson97@gmail.com>
2025-05-29 14:21:36 +0200

    Backend users update

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RegisterView
+from . import views
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', views.register_user, name='register_user'),
+    path('login/', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user'),
 ]
