ebde2a670f58991efe426f676e134d353b02c117
Topo0308 <toponelson97@gmail.com>
2025-06-02 16:37:31 +0200

    CREATION DES TRAJETS PAR USERS CONECTER

--- server/trajets/views.py
+++ server/trajets/views.py
@@ -24,8 +24,10 @@ def list_trajets(request):
 
 
 @csrf_exempt
-@login_required
 def create_trajet(request):
+    if not request.user.is_authenticated:
+        return JsonResponse({"error": "Authentification requise"}, status=401)
+
     if request.method == "POST":
         data = json.loads(request.body)
         trajet = Trajet.objects.create(
@@ -37,7 +39,9 @@ def create_trajet(request):
             conducteur=request.user
         )
         return JsonResponse({"message": "Trajet créé", "id": trajet.id})
+    
     return JsonResponse({"error": "Méthode non autorisée"}, status=405)
+
 
 
 @csrf_exempt
